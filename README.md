7902dcfc81e4de27829bdaf615f653f1de119446
Wesley Evangelista <wesley.pereira@medway.com.br>
2023-04-30 14:49:38 -0300

    added Cors

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Project_Outlet_backend
+
 Backend for Outlet to University
+Django + Django Rest Framework
+
+Rodando o projeto
+
+NECESSÁRIO PYTHON INSTALADO
+
+Após clonar o repositório, navegue para o caminho principal
+
+    $ cd project_outlet_backend
+
+Utilize o comando para rodar o servidor:
+
+    $ python3 manage.py runserver localhost:8000
+
+Criar usuários no Admin:
+
+    $ python3 manage.py createsuperuser
--- outlet/settings.py
+++ outlet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*y+p*$2^3@&8is5=j@e4#to$k_1$$=-12%vvruxh3)4_l$eu)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -41,17 +41,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'outlet.urls'
 
