702822f988bf96795dccb8ed3792462dcb731930
wesley <wesley.pereira@medway.com.br>
2023-06-10 09:32:35 -0300

    added filter and FK rating - shoes

--- outlet/settings.py
+++ outlet/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
--- shoes/models.py
+++ shoes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 def get_image_filename(self, filename):
         ext = filename.split('.')[-1]
         new_filename = f"{self.shoes_name}.{ext}"
@@ -9,7 +10,7 @@ class Shoes(models.Model):
     shoes_name = models.CharField(max_length= 50, default='Generico')
     brand = models.CharField(max_length=50)
     gender = models.CharField(max_length = 20)
-    size = models.IntegerField
+    size = models.JSONField(default=[])
     price = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     photo = models.ImageField(upload_to=get_image_filename,default=None, null=True, blank=True)
     
