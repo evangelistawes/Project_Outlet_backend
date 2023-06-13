0a92508ebfe09f1d12d7836b9a437ef8892fbecb
wesley <wesley.pereira@medway.com.br>
2023-06-13 20:07:12 -0300

    added author_name

--- rating/api/serializer.py
+++ rating/api/serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from rating.models import Rating
 
 class RatingSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(source='author.username')
+
     class Meta:
         model= Rating
         fields = '__all__'
